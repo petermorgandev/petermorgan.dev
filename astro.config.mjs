566c74dbf13f67bf8d7bec8d020a6ebc375b9616
Peter Morgan <peter@1080peter.com>
2022-09-08 00:13:42 -0600

    Update Astro config: requires trailing slash

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
 	site: 'https://petermorgan.dev',
+	trailingSlash: 'always',
 	markdown: {
     shikiConfig: {
 			wrap: true
